cff69760b94bc535b280d37cffedd47104bf67f8
junho <yunjunhojj@gmail.com>
2023-04-06 20:33:09 +0900

    header 완성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import Logo from "../assets/logo.svg";
+
 const Header = () => {
   return (
-    <div className="section" id="header">
-      Header
-    </div>
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-start">
+          <a href="#home">
+            <img src={Logo} alt="Logo" />
+          </a>
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
   );
 };
 
