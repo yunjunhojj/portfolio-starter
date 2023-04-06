7c23996ef285c79c8fff1655b5bffafcd25db83e
junho <yunjunhojj@gmail.com>
2023-04-06 18:33:35 +0900

    nav 설정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="section" id="header">
+      Header
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React from "react";
+
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+
+import { Link } from "react-scroll";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+      <div className="container mx-auto">
+        <div
+          className="w-full bg-black/20 h-[96px] backdrop-blur-2x1 
+        rounded-full max-w-[460px] mx-auto px-5 justify-between items-center flex text-2x1 text-whith/50"
+        >
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center "
+            to="home"
+            smooth={true}
+            duration={500}
+          >
+            <BiHomeAlt />
+          </Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center "
+            to="about"
+            smooth={true}
+            duration={500}
+          >
+            <BiUser />
+          </Link>
+
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center "
+            to="services"
+            smooth={true}
+            duration={500}
+          >
+            <BsClipboardData />
+          </Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center "
+            to="work"
+            smooth={true}
+            duration={500}
+          >
+            <BsBriefcase />
+          </Link>
+          <Link
+            className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center "
+            to="contact"
+            smooth={true}
+            duration={500}
+          >
+            <BsChatSquare />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
