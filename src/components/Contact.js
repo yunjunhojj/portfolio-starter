88a88a1cbf0e9ae553cdd17284ec9e0709bff785
junho <yunjunhojj@gmail.com>
2023-04-06 22:26:30 +0900

    contact 대충 완료

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,46 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
 const Contact = () => {
   return (
-    <div className="section" id="contact">
-      Contact
-    </div>
+    <section className="section" id="contact">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <motion.div
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            animate={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex justify-start items-center "
+          >
+            <div>
+              <h4 className="text-x1 uppercase text-accent font-medium mb-2 tracking-wide">
+                Get in touch
+              </h4>
+              <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+                Let's work <br /> together.
+              </h2>
+            </div>
+          </motion.div>
+          <div className="flex-1">
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Name"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Name"
+            />
+            <textarea></textarea>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
